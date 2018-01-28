844031785a87dd2a1aacf369f0fff2cb5bdcc7ee
dillon <dhall@motocol.com>
2018-01-28 14:50:34 -0700

    added function for finding rotation

--- robot_drive/robot_drive.ino
+++ robot_drive/robot_drive.ino
@@ -3,14 +3,21 @@
  * 
  */
 
+// needed for the chasses 
 #include <movestack.h>
 #include <move.h>
 #include <robotnav.h>
 #include <PRIZM.h>
 #include <assert.h>
+// needed for the gyroscope 
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
 
 PRIZM prizm; // object for driving the prizm chassis 
 move_stack moves; // stack to keeping track of moves that the robot has made 
+Adafruit_BNO055 bno = Adafruit_BNO055();
 
 void setup() 
 {
@@ -26,12 +33,25 @@ void setup()
 
 void loop() 
 {
-
-
+  
 }
 
 // rotate and drive a distance 
 void drive_vec(vmove& path)
+{
+  // get an angle from the gyroscope and rotate to match that angle
+  // figure out how many encoder ticks its going to be to 
+}
+
+// return rotation of the IMU on the x axis 
+float getAngle()
+{
+  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  return euler.x();
+}
+
+// sets up ctc interupt 
+void setup_interupts()
 {
   
 }
