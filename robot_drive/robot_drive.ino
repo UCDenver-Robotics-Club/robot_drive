9affb9963b62484490bda97ff178f2c0644072ca
dillon <dhall@motocol.com>
2018-03-22 15:33:59 -0600

    started adding code to the main robot project

--- robot_drive/robot_drive.ino
+++ robot_drive/robot_drive.ino
@@ -1,58 +1,46 @@
-/*
- * line sensors on page 150 of api manual 
- * 
- */
-
-// needed for the chasses 
-#include <movestack.h>
-#include <move.h>
-#include <robotnav.h>
+// includes
+#include <PID_v1.h>
 #include <PRIZM.h>
-#include <assert.h>
-// needed for the gyroscope 
-#include <Wire.h>
+#include <i2cdevice.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
+#include <ArduinoJson.h>
 
-PRIZM prizm; // object for driving the prizm chassis 
-move_stack moves; // stack to keeping track of moves that the robot has made 
-Adafruit_BNO055 bno = Adafruit_BNO055();
 
-void setup() 
+// program for test driving
+PRIZM prizm; // prizm object 
+Adafruit_BNO055 bno = Adafruit_BNO055(); // IMU object
+Ledblink blinker(8); // led blinker for status 
+
+void setup()
 {
-  // init the prizim lib
-  prizm.PrizmBegin();
-  prizm.setMotorInvert(1,1);
-  // setup the move stack 
-  init_stack(&moves);
-  // start the serial
-  Serial.begin(9600);
-  Serial.println("device started");
-}
+  Serial.begin(9600); // start of the serial port 
+  prizm.PrizmBegin(); // start up the prizm board and wait for go 
 
-void loop() 
-{
+  // start up the bno and make sure that it connected
+  if(!bno.begin())
+  {
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    //while(1);
+    delay(10000); // delay for 10 seconds so the OP can know what happend 
+  }
+
+  // setup the motors 
+  prizm.setMotorInvert(1,1); // invert motor #1 
+  prizm.setServoPosition(2,0); // turn on the convair belt
   
 }
 
-// rotate and drive a distance 
-void drive_vec(vmove& path)
+void loop()
 {
-  // get an angle from the gyroscope and rotate to match that angle
-  // figure out how many encoder ticks its going to be to 
+  Serial.println(getRotation());
+  
 }
 
-// return rotation of the IMU on the x axis 
-float getAngle()
+float getRotation()
 {
   imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
   return euler.x();
-}
-
-// sets up ctc interupt 
-void setup_interupts()
-{
-  
 }
 
