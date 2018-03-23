99725d29c349a0ff614f91447bd742f97f577ac8
dillon <dhall@motocol.com>
2018-03-22 18:30:07 -0600

    vector control added, but no pid

--- robot_drive/robot_drive.ino
+++ robot_drive/robot_drive.ino
@@ -6,17 +6,27 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
-//#include <ArduinoJson.h> 
+// constents 
+const int startingSpeed = 25;
 
 
 // objects for running the system
 PRIZM prizm; // prizm object 
 Adafruit_BNO055 bno = Adafruit_BNO055(); // IMU object
 Ledblink blinker(8); // led blinker for status 
+// vectors for navigation
 NavVector pos(0,0); // inital position
+NavVector motorPower(0,0);
+// position for the robot to drive to
+
+
 // powerlevles for the left and right motor
 int motor1,motor2;
-// set points and error for driving in a strait line
+// set points and error for driving in a line
+double setPoint = 0;
+double error=0;
+double correction=0;
+PID directionCorrection(&error,&correction,&setPoint,1,0,0,DIRECT);
 
 void setup()
 {
@@ -35,20 +45,35 @@ void setup()
   prizm.setMotorInvert(1,1); // invert motor #1 
  // prizm.setServoPosition(2,0); // turn on the convair belt
 
- // set the initial values for the motors
- motor1 = 20;
- motor2 = 20;
- prizm.setMotorPowers(motor1,motor2);
-  
+  // set the initial values for the motors
+  motorPower.setValues(startingSpeed,startingSpeed);
+  directionCorrection.SetMode(AUTOMATIC); // turn the pid control on
+
+  blinker.off(); // make sure that the status light is off
 }
 
 void loop()
 {
-  Serial.println(getRotation());
-  blinker.on();
-  delay(200);
-  blinker.off();
-  delay(200);
+//  Serial.println(getRotation());
+//  blinker.on();
+//  delay(100);
+//  Serial.println(getRotation());
+//  blinker.off();
+//  delay(100);
+
+  float theta = getRotation();
+  if(theta > 5)
+  {
+    blinker.on();
+  }
+  else
+  {
+    blinker.off();
+  }
+  Serial.println(theta);
+
+  // update the speed of the motors 
+  prizm.setMotorPowers(motorPower.getX(),motorPower.getY()); // set the motor speeds based on a vector 
 }
 
 float getRotation()
